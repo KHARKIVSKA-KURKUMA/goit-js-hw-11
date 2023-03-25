2737b25634d15ea647db6e2b4c80b579e1642249
Kateryna <catherine.vasyuk11@gmail.com>
2023-03-26 00:45:11 +0200

    done task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,12 @@
 async function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1';
-  const response = await fetch(`${BASE_URL}/name/${name}`);
+  const response = await fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  );
+  // console.log(response.json());
+  if (!response.ok) {
+    return [];
+  }
   return response.json();
 }
-
-// function fetchCountries(name) {
-//   return fetch(`${BASE_URL}/name/${name}`).then(response =>
-//     response.json()
-//   );
-// }
-
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import './css/styles.css';
 import CountryCardTpl from './small-country.hbs';
 import CountryDetailTpI from './country.hbs';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
+const Handlebars = require('handlebars');
+Handlebars.registerHelper('GOL', value => {
+  const lang = Object.values(value);
+  return lang;
+});
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,23 +19,57 @@ const refs = {
   info: document.querySelector('.country-info'),
 };
 
-fetchCountries('United Kingdom')
-  .then(renderSmallCountryCard)
-  .catch(console.log());
+refs.input.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 
-function renderSmallCountryCard([data]) {
+function onInputCountry(e) {
+  let inputCountry = e.target.value.trim();
+  if (inputCountry === '') {
+    return;
+  }
+
+  hideCountryDetail();
+  hideCountriesList();
+
+  fetchCountries(inputCountry)
+    .then(countries => {
+      if (countries.length === 0) {
+        showError();
+      } else if (countries.length === 1) {
+        renderDetailCountryCard(countries);
+      } else if (countries.length <= 10 && countries.length > 1) {
+        renderSmallCountryCards(countries);
+      } else if (countries.length > 10) {
+        showMessage();
+      }
+    })
+    .catch(console.error);
+}
+function renderSmallCountryCards(data) {
+  data.forEach(country => {
+    const markup = CountryCardTpl(country);
+    refs.list.innerHTML += markup;
+  });
+}
+function renderDetailCountryCard([data]) {
   const markup = CountryDetailTpI(data);
   refs.list.innerHTML = markup;
 }
-
-/* -------------------------------------------------------------------------- */
-
-// (async () => {
-//   try {
-//     const [data] = await fetchCountries('United Kingdom');
-//     const markup = CountryCardTpl(data);
-//     refs.list.innerHTML = markup;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
+function hideCountriesList() {
+  refs.list.innerHTML = '';
+}
+function hideCountryDetail() {
+  refs.list.innerHTML = '';
+}
+function showError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name', {
+    timeout: 700,
+  });
+}
+function showMessage() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.',
+    {
+      timeout: 700,
+    }
+  );
+}
